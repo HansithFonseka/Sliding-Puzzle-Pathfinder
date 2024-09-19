7c2a59068cf632309d5e06dc16e575fa364af853
Hansith <hansithlochana@gmail.com>
2024-09-19 10:48:16 +0530

    Add Places class

--- Places.java
+++ Places.java
@@ -1,3 +1,27 @@
 public class Places {
-    
+    private final int rowNumber;
+    private final int columnNumber;
+    private Places parent;
+    public String direction;
+
+    public Places(int rowNumber, int columnNumber) {
+        this.columnNumber = columnNumber;
+        this.rowNumber = rowNumber;
+    }
+
+    public Places getParent() {
+        return parent;
+    }
+
+    public void setParent(Places parent) {
+        this.parent = parent;
+    }
+
+    public int getRowNumber() {
+        return rowNumber;
+    }
+
+    public int getColumnNumber() {
+        return columnNumber;
+    }
 }
